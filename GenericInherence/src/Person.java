2c18374b20b165692f4ff72724093cd35fa919f1
Verenich Tatiana <verenik91@gmail.com>
2024-01-13 23:36:11 +0300

    урок 5, иерархия обобщенных типов

---
+++ GenericInherence/src/Person.java
@@ -0,0 +1,34 @@
+
+
+public class Person<T,V> {
+    private T name;
+    private V age;
+
+    public Person(T name, V age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public T getName() {
+        return name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+    public V getAge() {
+        return age;
+    }
+
+    public void setAge(V age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User {Name='" + name + "', Age=" + age + "}";
+    } 
+
+
+}
